8143174c4b2b4d9d0dc90e985b24cbd0486e53a9
rich lee <rich@richde-MacBook-Pro.local>
2020-11-14 15:49:08 +0800

    init book-service project

---
+++ book-service/src/test/java/com/rich/quarkus/NativeBookResourceIT.java
@@ -0,0 +1,9 @@
+package com.rich.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeBookResourceIT extends BookResourceTest {
+
+    // Execute the same tests but in native mode.
+}
