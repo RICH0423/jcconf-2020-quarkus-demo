209c3ed9660089a7d6deb0114552c48bf689bb6d
rich lee <rich@richde-MacBook-Pro.local>
2020-11-17 20:20:07 +0800

    add unit tests for UserResource

--- user-service/src/test/java/com/rich/quarkus/UserResourceTest.java
+++ user-service/src/test/java/com/rich/quarkus/UserResourceTest.java
@@ -1,21 +1,50 @@
 package com.rich.quarkus;
 
+import com.rich.quarkus.repository.User;
+import com.rich.quarkus.repository.UserRepository;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @QuarkusTest
 public class UserResourceTest {
 
-//    @Test
-//    public void testHelloEndpoint() {
-//        given()
-//          .when().get("/users")
-//          .then()
-//             .statusCode(200)
-//             .body(is("hello"));
-//    }
+    @InjectMock
+    UserRepository userRepository;
 
+    @Test
+    public void whenGetAllUsers_thenBodyShouldNotBeEmpty() {
+        when(userRepository.getAll()).thenReturn(
+                List.of(new User("rich", 30)));
+
+        given()
+          .when().get("/users")
+          .then()
+             .statusCode(200)
+             .body(is(not(empty())));
+    }
+
+    @Test
+    public void whenCreateUser_thenShouldReturnSuccessfully() {
+        User user = new User("rich", 30);
+        doNothing().when(userRepository).create(user);
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(user)
+                .when().post("/users")
+                .then()
+                .statusCode(200)
+                .body(is(not(empty())));
+    }
 }
